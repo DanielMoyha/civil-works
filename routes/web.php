dd9c987b20fb6f74a3e67649e2c3064c8077c132
Daniel Moya <dnmoyha@gmail.com>
2023-01-14 21:19:23 -0400

    add route for cleaning cache

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\StudyController;
 use App\Http\Controllers\SupervisionController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WorkController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 
@@ -95,6 +96,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::get('/route-cache', function() {
+	Artisan::call('route:cache');
+    return 'Routes cache has been cleared';
 });
 
 require __DIR__.'/auth.php';
